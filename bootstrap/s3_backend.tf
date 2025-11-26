2b3913ce3f27837298541845e511e900a74982c9
sbhalsing99 <bhalsing0899@gmail.com>
2025-11-27 00:39:11 +0530

    modify 3

--- bootstrap/s3_backend.tf
+++ bootstrap/s3_backend.tf
@@ -2,32 +2,31 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "tf_state_bucket" {
+resource "aws_s3_bucket" "tf_backend" {
   bucket = "shruti-tf-backend-bucket"
+  force_destroy = false
 }
 
 resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.tf_state_bucket.id
+  bucket = aws_s3_bucket.tf_backend.id
 
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket_encryption" "encryption" {
-  bucket = aws_s3_bucket.tf_state_bucket.id
+resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
+  bucket = aws_s3_bucket.tf_backend.id
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
 }
 
 resource "aws_dynamodb_table" "tf_lock" {
-  name         = "terraform-state-lock"
+  name         = "terraform-lock-table"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
