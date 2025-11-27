a7c9bbf236c548a638244d0f724622d677ba3331
sbhalsing99 <bhalsing0899@gmail.com>
2025-11-27 13:06:04 +0530

    modify 19

--- ec2/backend.tf
+++ ec2/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "shruti-tf-backend-bucket"
-    key            = "main/terraform.tfstate"
+    key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-state-lock-table"
     encrypt        = true
