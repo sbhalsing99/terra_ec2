0281a2858080ab3a74a02df1dae0a1e1517bbc81
sbhalsing99 <bhalsing0899@gmail.com>
2025-11-27 12:52:46 +0530

    modify 16

--- remote-infra/S3.tf
+++ remote-infra/S3.tf
@@ -2,13 +2,4 @@ resource "aws_s3_bucket" "tf_backend_bucket" {
   bucket = "shruti-tf-backend-bucket"
 }
 
-resource "aws_dynamodb_table" "tf_state_lock" {
-  name         = "terraform-state-lock-table"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
