53a89fbff06894182be78dfdeb858478b0dbd883
sbhalsing99 <bhalsing0899@gmail.com>
2025-11-27 12:47:14 +0530

    modify 15

--- remote-infra/terraform.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "shruti-tf-backend-bucket"
-    key            = "ec2/terraform.tfstate"
-    region         = "us-east-1"
-    use_lockfile   = true
-  }
-}
